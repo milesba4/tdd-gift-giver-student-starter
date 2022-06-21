c3cecdb1a2283ede0db46009eefeee3b0da55102
milesba4 <milesbaffour@gmail.com>
2022-06-21 14:50:37 -0400

    creating a get request handler at "/" endpoint

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
+const router = express.Router();
+
 module.exports=app // exporting app to server.js
 app.use(morgan("tiny"))
 app.use(express.json())
+app.use("/", router);
+
+router.get('/', (req, res) => {
+    res.send({ "ping": "pong" },"status:200")
+  })
