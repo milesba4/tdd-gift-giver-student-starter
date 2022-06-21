43d8dcd019d9523b81bc38b9f1698d8a5d113075
milesba4 <milesbaffour@gmail.com>
2022-06-21 14:32:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require("express")
 const app = express()
-module.exports=app
-
 const morgan = require("morgan")
 module.exports=app // exporting app to server.js
 app.use(morgan("tiny"))
