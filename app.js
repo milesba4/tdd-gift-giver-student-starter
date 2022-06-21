046e657774ac791d579c716c0b2d191018cb290a
milesba4 <milesbaffour@gmail.com>
2022-06-21 14:31:45 -0400

    incorporating a logging middleware into the application with the morgan package and setting its logging level to tiny

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ const express = require("express")
 const app = express()
 module.exports=app
 
+const morgan = require("morgan")
 module.exports=app // exporting app to server.js
+app.use(morgan("tiny"))
