fe0c1ccd1d9ec9c99b570066617e83e831f8f1c0
milesba4 <milesbaffour@gmail.com>
2022-06-21 21:59:18 -0400

    creating a .get handler for default route

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
-const router = express.Router();
 
-module.exports=app // exporting app to server.js
+module.exports=app // exporting app as default
 app.use(morgan("tiny"))
 app.use(express.json())
-app.use("/", router);
-
-router.get('/', (req, res) => {
-    res.send({ "ping": "pong" },"status:200")
+app.get('/', (req, res) => {
+    res.status(200).send({ "ping": "pong" })
   })
